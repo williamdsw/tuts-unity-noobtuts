8d3cfdd60d1f57db71274584fcbcd7a61a64f220
William Santos <williamdsw@outlook.com>
2020-12-13 16:23:07 -0300

    Uploading Snake clone

---
+++ snake-clone/Assets/Scripts/Snake.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private GameObject tailPrefab;
+    private float timeToStart = 0.3f;
+    private float timeToRepeat = 0.1f;
+    private Vector2 direction = Vector2.right;
+
+    // State
+    private List<Transform> tails = new List<Transform> ();
+    private bool hasAte = false;
+
+    private void Start () 
+    {
+        InvokeRepeating ("Move", timeToStart, timeToRepeat);
+    }
+
+    private void Update () 
+    {
+        ControlDirection ();
+    }
+
+    private void OnTriggerEnter2D (Collider2D other) 
+    {
+        if (other.name.StartsWith ("Food"))
+        {
+            hasAte = true;
+            Destroy (other.gameObject);
+        }
+        else 
+        {
+            Debug.Log ("You lose!");
+        }
+    }
+
+    // Controls current direction with keys
+    private void ControlDirection ()
+    {
+        if (Input.GetKey (KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+        else if (Input.GetKey (KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+        else if (Input.GetKey (KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKey (KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+        }
+    }
+
+    private void Move ()
+    {
+        // Current position
+        Vector2 position = this.transform.position;
+
+        // Moves to next position
+        transform.Translate (direction);
+
+        if (hasAte)
+        {
+            GameObject tail = Instantiate (tailPrefab, position, Quaternion.identity) as GameObject;
+            tails.Insert (0, tail.transform);
+            hasAte = false;
+        }
+        else if (tails.Count > 0)
+        {
+            // Updates position
+            tails.Last ().position = position;
+
+            // Set positions on list
+            tails.Insert (0, tails.Last ());
+            tails.RemoveAt (tails.Count - 1);
+        }
+    }
+}
---
+++ snake-clone/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    [SerializeField] private GameObject foodPrefab;
+    [SerializeField] private Transform borderTop;
+    [SerializeField] private Transform borderBottom;
+    [SerializeField] private Transform borderLeft;
+    [SerializeField] private Transform borderRight;
+    private float timeToStart = 3f;
+    private float timeToRepeat = 4f;
+
+    private void Start () 
+    {
+        // Calls the method first at "timeToStart" and repeat at "timeToRepeat"
+        InvokeRepeating ("Spawn", timeToStart, timeToRepeat);
+    }
+
+    // Spawns the food at random position
+    private void Spawn ()
+    {
+        int x = (int) Random.Range (borderLeft.position.x, borderRight.position.x);
+        int y = (int) Random.Range (borderBottom.position.y, borderTop.position.y);
+        Instantiate (foodPrefab, new Vector3 (x, y, 0), Quaternion.identity);
+    }
+}
