4c5d4f71575f633e52363e1ddfcf299d97bfe727
William Santos <williamdsw@outlook.com>
2020-12-13 16:13:57 -0300

    Uploading Pong clone

---
+++ pong-clone/Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float ballSpeed = 3000f;
+    private Rigidbody2D rigidBody2D;
+
+    private void Awake () 
+    {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Start () 
+    {
+        rigidBody2D.velocity = (Vector2.right * ballSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D (Collision2D other) 
+    {
+        string otherName = other.gameObject.name;
+        if (otherName.StartsWith("Racket"))
+        {
+            float x = (otherName.Equals("RacketLeft") ? 1 : -1);
+            float yHitFactor = HitFactor (this.transform.position, other.transform.position, other.collider.bounds.size.y);
+            Vector2 newDirection = new Vector2 (x, yHitFactor).normalized;
+            rigidBody2D.velocity = (newDirection * ballSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    // Calculates hit factor with rackets
+    private float HitFactor (Vector2 ballPosition,  Vector2 racketPosition, float racketHeight)
+    {
+        return (ballPosition.y - racketPosition.y) / racketHeight;
+    }
+}
---
+++ pong-clone/Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    private float moveSpeed = 3000f;
+    [SerializeField] private string verticalAxis = "Vertical";
+    private Rigidbody2D rigidBody2D;
+
+    private void Awake () 
+    {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate () 
+    {
+        float vertical = Input.GetAxisRaw (verticalAxis);
+        rigidBody2D.velocity = (new Vector2 (0, vertical) * moveSpeed * Time.fixedDeltaTime);
+    }
+}
